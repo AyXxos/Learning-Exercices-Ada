fa4c992f8ba1219c368cab0a5b3a657b0d9669e6
AyXxos <122178037+AyXxos@users.noreply.github.com>
2026-01-14 21:28:18 +0100

    Fix style warnings in 'inoutparameters.adb'

--- inoutparameters/src/inoutparameters.adb
+++ inoutparameters/src/inoutparameters.adb
@@ -1,26 +1,25 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Float_Text_IO; use Ada.Float_Text_IO; 
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 
 procedure Inoutparameters is
-   
-   procedure Display_Money (Account : in Float) is
+   procedure Display_Money (Account : Float) is
    begin
-      Put_Line ("You currently have :" & Float'Image(Account));
+      Put_Line ("You currently have :" & Float'Image (Account));
    end Display_Money;
 
-   procedure Add_To_Account (Account : in out Float; Amount : in Float) is
+   procedure Add_To_Account (
+      Account : in out Float;
+      Amount : Float) is
    begin
       Account := Account + Amount;
    end Add_To_Account;
 
-   procedure Apply_Financial_Interests (Account : in out Float;  Rate : in Float) is
-   Gain : Float;
+   procedure Apply_Financial_Interests (Account : in out Float; Rate : Float) is
+      Gain : Float;
    begin
       Gain := Account * Rate;
       Add_To_Account (Account, Gain);
    end Apply_Financial_Interests;
-
-
 
    my_account : Float := 1000.0;
    bonus : Float := 0.05;
