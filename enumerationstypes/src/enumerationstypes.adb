250ca349095f284c64d3f593d0a393b7272c4310
AyXxos <122178037+AyXxos@users.noreply.github.com>
2026-02-03 21:49:09 +0100

    Add a subtype for the food items in the backpack

--- enumerationstypes/src/enumerationstypes.adb
+++ enumerationstypes/src/enumerationstypes.adb
@@ -2,11 +2,19 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Enumerationstypes is
    type Backpack is (
-      Water, Sausage, Knife,
+      Water, Sausage, Cereals, Knife,
       Socks);
+
+   subtype Food is
+      Backpack range Water .. Cereals;
 begin
    Put_Line ("In my backpack there is ");
    for I in Backpack loop
-      Put_Line (Backpack'Image (I));
+      case I is
+         when Food =>
+            Put_Line ("Miam ! Some " & Backpack'Image (I));
+         when others =>
+            Put_Line ("Nice " & Backpack'Image (I));
+      end case;
    end loop;
 end Enumerationstypes;
