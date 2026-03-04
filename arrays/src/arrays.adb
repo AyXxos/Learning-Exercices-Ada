1627d5dfae0b41340c4956dca0cb77147e8ed4f1
Adam Sicaud <adamsicaud@gmail.com>
2026-03-04 16:48:59 +0100

    Add array length

--- arrays/src/arrays.adb
+++ arrays/src/arrays.adb
@@ -2,18 +2,20 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Arrays is
    type My_Int is range 0 .. 500;
-   type Bounds is range 1 .. 5;
+   type Bounds is range My_Int'First .. My_Int'First + 4;
 
    type My_Int_Array is
       array (Bounds) of My_Int;
 
-   Arr : constant My_Int_Array := (2, 3 , 300, 200, 51);
+   Arr : constant My_Int_Array := (2, 3, 300, 200, 51);
 
    V : My_Int;
 begin
    for I in Bounds loop
       V := Arr (I);
       Put_Line (My_Int'Image (V));
+      
    end loop;
+   Put_Line ("Array length is " & My_Int_Array'Length'Image);
    New_Line;
 end Arrays;
