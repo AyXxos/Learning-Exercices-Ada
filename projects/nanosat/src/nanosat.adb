7633fb7b606db823919bc340634f25124eab46a2
Adam Sicaud <adamsicaud@gmail.com>
2026-03-11 17:03:05 +0100

    Show that we can access the informations using functions

--- projects/nanosat/src/nanosat.adb
+++ projects/nanosat/src/nanosat.adb
@@ -4,4 +4,9 @@ with satellite_infos; use satellite_infos;
 procedure Nanosat is
 begin
    Display_Satellite_Infos;
+   Put_Line ("");
+   Put_Line ("You can also access the satellite information using functions:");
+   Put_Line ("Satellite Name: " & Get_Satellite_Name);
+   Put_Line ("Launch Date: " & Get_Launch_Date);
+
 end Nanosat;
